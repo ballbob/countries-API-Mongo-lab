97fd9cdcea1b0f36d56e994eb3dd4bc671b34f33
Robert Ball <robert.ball.1989@gmail.com>
2017-04-19 15:41:22 +0100

    Adds to local API and Mongo.

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,4 @@
 var CountryFetcher = require('./models/CountryFetcher.js')
-var CountryQuery = require('/../db/countryQuery.js')
 
 function populateSelect(countries){
   console.log(countries)
@@ -17,19 +16,53 @@ var app = function(){
   var fetcher = new CountryFetcher("https://restcountries.eu/rest/v2/all")
   fetcher.fetch(populateSelect);
 
-  var countryQuery = new CountryQuery()
-
   var button = document.querySelector('#submit')
   var dropdown = document.querySelector('#countries-dropdown')
 
-  button.addEventListener = ('click', function(){
-
+  button.addEventListener('click', function(){
+    saveCountry(dropdown.value);
   })
 }
 
+var saveCountry = function(countryString){
+  var request = new XMLHttpRequest();
+  request.open("POST", "http://localhost:3000/countries");
+  request.setRequestHeader("Content-Type", "application/json")
+  request.onload = function(){
+    //addToList(countryString)
+    // CALL A FUNCTION TO CREATE A LIST IN THE DOM
+    populateList()
+  }
+  request.send(JSON.stringify({countryString}));
+}
 
+var addToList = function(item){
+  var listedCountries = document.querySelector('#bucket-list')
 
+  var countryP = document.createElement('p')
+  countryP.innerText = item
 
+  listedCountries.appendChild(countryP)
+}
+
+var populateList = function(){
+  var request = new XMLHttpRequest()
+
+  request.open('GET',"http://localhost:3000/countries")
+
+  request.onload = function(){
+    if(request.status === 200){
+      var jsonString = request.responseText
+      var selectedCountries = JSON.parse(jsonString)
+
+      for (var i=0; i < selectedCountries.length; i++){
+        var listCountryP = document.createElement('p')
+        listCountryP.innerText = selectedCountries[i].countryString
+      }
+
+    }
+  }
+}
 
 
 window.onload = app
